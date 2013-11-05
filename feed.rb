3a80866b371b6f4443f303ab1e0532ac5183e090
nyi <serge@nyi.ch>
2013-11-05 18:26:16 +0100

    Changed to use open-uri Added parsing of CSV

--- feed.rb
+++ feed.rb
@@ -1,13 +1,48 @@
-require 'net/http'
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+require 'rss'
 
-resource = Net::HTTP.new('epguides.com', 80)
-headers,data = resource.get('/common/exportToCSV.asp?rage=8511')
+class Show
+  attr_accessor :season, :episode, :name, :date
 
-print "HERE IS THE FILE CONTENT\n"
-print data
-
-print "\nHEADERS:\n"
-headers.each do |key,val|
-  print "#{key}: #{val[0..50]}\n"
+  @season
+  @episode
+  @name
+  @date
 end
-print "Status code #{headers.code}, message #{headers.message}\n"
+
+shows = Array.new
+
+htmlPage = Nokogiri::HTML(open('http://www.epguides.com/common/exportToCSV.asp?rage=8511'))
+
+htmlPage.xpath('//pre').each do |pre|
+  csvContent = pre.content
+  csvContent.strip!
+
+  arr_of_arrs = CSV.parse(csvContent)
+
+  arr_of_arrs.shift
+
+  arr_of_arrs.each do |entry|
+    season = entry[1]
+    episode = entry[2]
+    date = entry[4]
+    name = entry[5]
+
+    show = Show.new
+    show.season = season
+    show.episode = episode
+    show.date = date
+    show.name = name
+
+    shows << show
+  end
+
+  puts 'number of entries: ', arr_of_arrs.count
+
+  shows.each do |show|
+    print show.season, '/', show.episode, ': ', show.name, ' ', show.date
+    puts
+  end
+end
