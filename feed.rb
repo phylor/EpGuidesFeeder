781b7375dcc2c55c3c77519111217807bece121b
nyi <serge@nyi.ch>
2013-11-05 20:15:24 +0100

    Some finetuning

--- feed.rb
+++ feed.rb
@@ -12,54 +12,66 @@ class Show
   @date
 end
 
-shows = Array.new
+class EpGuideFeed
+  @shows
 
-htmlPage = Nokogiri::HTML(open('http://www.epguides.com/common/exportToCSV.asp?rage=8511'))
-
-htmlPage.xpath('//pre').each do |pre|
-  csvContent = pre.content
-  csvContent.strip!
-
-  arr_of_arrs = CSV.parse(csvContent)
-
-  arr_of_arrs.shift
-
-  arr_of_arrs.each do |entry|
-    season = entry[1]
-    episode = entry[2]
-    date = entry[4]
-    name = entry[5]
-
-    if date != 'UNAIRED'
-     show = Show.new
-     show.season = season
-     show.episode = episode
-     show.date = date
-     show.name = name
-
-     shows << show
-    end
+  def initialize
+    @shows = Array.new
   end
 
-  #shows.each do |show|
-  #  print show.season, '/', show.episode, ': ', show.name, ' ', show.date
-  #  puts
-  #end
+  def get_feed(seriesName, epGuideId)
+    htmlPage = Nokogiri::HTML(open('http://www.epguides.com/common/exportToCSV.asp?rage='+epGuideId.to_s))
+    
+    htmlPage.xpath('//pre').each do |pre|
+      csvContent = pre.content
+      csvContent.strip!
+    
+      arr_of_arrs = CSV.parse(csvContent)
+    
+      arr_of_arrs.shift
+    
+      arr_of_arrs.each do |entry|
+        season = entry[1]
+        episode = entry[2]
+        date = entry[4]
+        name = entry[5]
+    
+        if date != 'UNAIRED'
+         show = Show.new
+         show.season = "%02d" % season
+         show.episode = "%02d" % episode
+         show.date = date
+         show.name = name
+    
+         @shows << show
+        end
+      end
+    
+      #todo: debug
+      #shows.each do |show|
+      #  print show.season, '/', show.episode, ': ', show.name, ' ', show.date
+      #  puts
+      #end
+    
+      rss = RSS::Maker.make("atom") do |maker|
+        maker.channel.author = ''
+        maker.channel.updated = Time.now.to_s
+        maker.channel.about = ''
+        maker.channel.title = seriesName
+    
+        @shows.each do |show|
+           maker.items.new_item do |item|
+             item.link = "https://www.google.com/#q=#{URI::encode seriesName}+s#{show.season}e#{show.episode}+netload"
+             item.title = show.name
+             item.updated = show.date
+           end
+        end
+      end
 
-  rss = RSS::Maker.make("atom") do |maker|
-    maker.channel.author = "meta"
-    maker.channel.updated = Time.now.to_s # should be newest date from CSV
-    maker.channel.about = 'http://nyi.ch'
-    maker.channel.title = 'Episodes'
-
-    shows.each do |show|
-       maker.items.new_item do |item|
-         item.link = 'http://nyi.ch'
-         item.title = show.name
-         item.updated = show.date
-       end
+      return rss
     end
   end
-
-  puts rss
 end
+
+feed = EpGuideFeed.new
+puts feed.get_feed 'The Big Bang Theory', 8511
