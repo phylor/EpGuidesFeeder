544fda91739b45c10ac6b527736d2db0b24f14e0
nyi <serge@nyi.ch>
2013-11-06 23:17:12 +0100

    Removed hardcoded value

--- application.rb
+++ application.rb
@@ -8,12 +8,8 @@ get '/' do
 end
 
 get '/:name/:id' do
-  #feed = EpGuideFeed.new
-  #puts feed.get_feed params[:name], params[:id].to_i
-  #feed.get_feed 'hello', 8511
-
   feed = EpGuideFeed.new
-  feed_result = feed.get_feed 'The Big Bang Theory', 8511
+  feed_result = feed.get_feed params[:name], params[:id].to_i
 
   feed_result.to_s
 end
